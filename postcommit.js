4eadaa386899da0e4d2ef5d651ced39908b01b70
Javier Chávarri <javier.chavarri@gmail.com>
2017-02-19 20:51:29 +0100

    dsfsdf

--- postcommit.js
+++ postcommit.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-console.log('No bla bla bla');
+console.log('No bla bla');
 console.log('process.env.GIT_PARAMS');
 console.log(JSON.stringify(process.env.GIT_PARAMS));
 process.exit(1);
